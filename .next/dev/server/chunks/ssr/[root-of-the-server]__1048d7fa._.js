f0971a1b55d96f331ea3079e28e9fa36bb02cf32
Om <98145765+hOMie03@users.noreply.github.com>
2025-12-28 23:10:41 +0530

    added changes

--- .next/dev/server/chunks/ssr/[root-of-the-server]__1048d7fa._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__1048d7fa._.js
@@ -84,12 +84,12 @@ function RootLayout({ children }) {
             children: children
         }, void 0, false, {
             fileName: "[project]/src/app/layout.tsx",
-            lineNumber: 19,
+            lineNumber: 20,
             columnNumber: 13
         }, this)
     }, void 0, false, {
         fileName: "[project]/src/app/layout.tsx",
-        lineNumber: 18,
+        lineNumber: 19,
         columnNumber: 9
     }, this);
 }
